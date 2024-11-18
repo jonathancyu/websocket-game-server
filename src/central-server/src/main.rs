7eda8c11e2213534b9908a323ba0413f0f090d37
Jonathan Yu <yucjonathan@gmail.com>
2024-11-17 23:09:56 -0600

    feat: basic axum endpoint

--- src/central-server/src/main.rs
+++ src/central-server/src/main.rs
@@ -1,29 +1,29 @@
-use std::{
-    io::{BufRead, BufReader, Write},
-    net::{TcpListener, TcpStream},
+use axum::{
+    http::StatusCode,
+    routing::{get, post},
+    Json, Router,
 };
+use serde::Deserialize;
 
-use common::threadpool::ThreadPool;
-
-fn main() {
-    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
-
-    let pool = ThreadPool::new(8);
-    for stream in listener.incoming().take(10) {
-        let stream = stream.unwrap();
-        pool.execute(|| handle_connection(stream));
-    }
+#[tokio::main]
+async fn main() {
+    let app: Router = Router::new()
+        .route("/", get(root))
+        .route("/join_queue", post(join_queue));
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
 }
-fn handle_connection(mut stream: TcpStream) {
-    let buf_reader = BufReader::new(&mut stream);
-    let http_request: Vec<_> = buf_reader
-        .lines()
-        .map(|result| result.unwrap())
-        .take_while(|line| !line.is_empty())
-        .collect();
 
-    let response = "HTTP/1.1 200 OK\r\n\r\n";
+async fn root() -> &'static str {
+    "Hello world"
+}
 
-    stream.write_all(response.as_bytes()).unwrap();
-    println!("Request: {http_request:#?}");
+async fn join_queue(Json(payload): Json<QueueRequest>) -> (StatusCode, Json<String>) {
+    let result = format!("hi {}", payload.user_id);
+    (StatusCode::OK, Json(result))
+}
+
+#[derive(Deserialize)]
+struct QueueRequest {
+    pub user_id: u64,
 }
