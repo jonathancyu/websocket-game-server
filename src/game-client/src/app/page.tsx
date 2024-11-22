8462ce06be3436663efd420a45f9da5e76b8eea8
Jonathan Yu <yucjonathan@gmail.com>
2024-11-22 01:43:30 -0600

    feat: formatted, tried to make better

--- src/game-client/src/app/page.tsx
+++ src/game-client/src/app/page.tsx
@@ -1,7 +1,6 @@
 import Queue from "./queue/page";
 
 export default function Home() {
-
   return (
     <div className="m-5">
       Super serious game client
--- src/game-client/src/app/queue/page.tsx
+++ src/game-client/src/app/queue/page.tsx
@@ -1,59 +1,109 @@
-'use client';
+"use client";
 
 import { useEffect, useState } from "react";
 
 type SocketHandle = {
-    messages: string[],
-    sendMessage: (message: string) => void
+  messages: string[];
+  sendMessage: (message: string) => void;
 };
+
 const useWebSocket = (url: string) => {
-    const [messages, setMessages] = useState<string[]>([]);
-    const [ws, setWs] = useState<WebSocket | null>(null);
+  const [messages, setMessages] = useState<string[]>([]);
+  const [ws, setWs] = useState<WebSocket | null>(null);
 
-    useEffect(() => {
-        const socket = new WebSocket(url);
-        setWs(socket);
+  useEffect(() => {
+    const socket = new WebSocket(url);
+    setWs(socket);
 
-        socket.onmessage = (event) => {
-            setMessages((previous) => [...previous, event.data]);
-        }
+    socket.onmessage = (event) => {
+      setMessages((previous) => [...previous, event.data]);
+    };
 
-        return () => {
-            socket.close();
-        }
-    }, [url])
+    return () => {
+      socket.close();
+    };
+  }, [url]);
 
-    const sendMessage = (message: string) => {
-        if (ws) {
-            ws.send(message);
-        }
+  const sendMessage = (message: string) => {
+    if (ws) {
+      ws.send(message);
     }
+  };
 
-    return { messages, sendMessage };
-}
+  return { messages, sendMessage };
+};
+
+type GetServer = {
+  GetServer: {
+    user_id: string;
+  };
+};
+type JoinQueue = {
+  JoinQueue: {
+    user_id: string;
+  };
+};
 
 export default function Queue() {
+  const [user_id] = useState(() => crypto.randomUUID());
+  const [socketHandle, setSocketHandle] = useState<SocketHandle | null>(null);
+
+  function connect() {
     const { messages, sendMessage } = useWebSocket("ws://localhost:3001");
+    setSocketHandle({
+      messages: messages,
+      sendMessage: sendMessage,
+    });
+  }
 
-    function joinQueue() {
-        sendMessage("Test");
-        console.log("Sent test");
+  function getServer() {
+    if (socketHandle == null) {
+      console.log("Socket handle null");
+      return;
     }
+    const request: GetServer = {
+      GetServer: {
+        user_id: user_id,
+      },
+    };
+    const payload = JSON.stringify(request);
+    socketHandle.sendMessage(payload);
+    console.log("Sent " + payload);
+  }
 
+  function joinQueue() {
+    if (socketHandle == null) {
+      console.log("Socket handle null");
+      return;
+    }
+    const request: JoinQueue = {
+      JoinQueue: {
+        user_id: user_id,
+      },
+    };
+    const payload = JSON.stringify(request);
+    socketHandle.sendMessage(payload);
+    console.log("Sent " + payload);
+  }
 
-    return (
-        <div className="m-2">
-            <button className="bg-blue-50 text-black" onClick={joinQueue}>Join queue</button>
-            <div>
-                {
-                    messages.map((msg, index) => (
-                        <div key={index}>{msg}</div>
-                    ))
-                }
-            </div>
-
+  return (
+    <div className="m-2 space-x-10">
+      <button className="bg-blue-50 text-black" onClick={connect}>
+        Connect
+      </button>
+      <button className="bg-blue-50 text-black" onClick={joinQueue}>
+        Join queue
+      </button>
+      <button className="bg-blue-50 text-black" onClick={getServer}>
+        get server
+      </button>
+      {socketHandle && (
+        <div>
+          {socketHandle.messages.map((msg, index) => (
+            <div key={index}>{msg}</div>
+          ))}
         </div>
-
-    )
+      )}
+    </div>
+  );
 }
-
